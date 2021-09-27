17857fbc77a15cd8357d117ccb6871a698447903
simone-perego <simo_bryant@hotmail.it>
2021-09-27 16:21:04 +0200

    commit

--- spacex-launches-project/src/App.vue
+++ spacex-launches-project/src/App.vue
@@ -1,12 +1,32 @@
 <template>
+<div class="wrapper">
+  <nav id="sidebar">
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
+  </nav>
+</div>
 </template>
 
 <style>
+.wrapper {
+    display: flex;
+    width: 100%;
+}
+
+#sidebar {
+    width: 250px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    z-index: 999;
+    background: #7386D5;
+    color: #fff;
+    transition: all 0.3s;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
