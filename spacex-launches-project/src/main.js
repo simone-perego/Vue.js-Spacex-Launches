acea53021711d52bd1afd9f6fdf574ec1e88f90f
simone-perego <simo_bryant@hotmail.it>
2021-09-27 15:41:41 +0200

    commit

--- spacex-launches-project/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue"
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import './plugins/bootstrap-vue'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
