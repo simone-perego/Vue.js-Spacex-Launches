70aad62c55364091d484c4926fdb255d74e3d96a
simone-perego <simo_bryant@hotmail.it>
2021-09-27 13:58:01 +0200

    added api

--- spacex-launches/src/main.js
+++ spacex-launches/src/main.js
@@ -6,3 +6,10 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+import ApolloClient from 'apollo-boost'
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'https://api.spacex.land/graphql/'
+})
