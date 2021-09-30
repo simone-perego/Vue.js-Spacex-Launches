57a58d7d6b355c7c50eb14614c30ae0f0b299351
Simone Perego <77017709+simone-perego@users.noreply.github.com>
2021-09-30 15:14:26 +0200

    Update Launch.vue
    sistemata pagina dettaglio in main

--- spacex-launches/src/views/Launch.vue
+++ spacex-launches/src/views/Launch.vue
@@ -2,39 +2,64 @@
   <div class="about container">
     <div class="title">
       <h1>LAUNCH DETAIL</h1>
-      <p>id: {{ id }} </p>
-      <p>id: {{ launch_year }} </p>
-      <div v-for="launch in launch" :key="launch.id">
-        <p>mission: {{ launch.id }}</p>
-      </div>
+      <h2>{{ launch.mission_name }}</h2>
     </div>
-    <table class="table">
-  <tbody>
-  </tbody>
-</table>
+    <div class="detail">
+      <div v-if="launch">
+        <table class="table">
+          <thead>
+          <tr>
+          <th scope="col">NAME</th>
+          <th scope="col">MODEL</th>
+          <th scope="col">WEIGHT</th>
+          <th scope="col">YEAR BUILT</th>
+          </tr>
+        </thead> 
+        <tbody>  
+        <tr v-for="ship in launch.ships" :key="ship.name">
+          <td>{{ ship.name }}</td>
+          <td>{{ ship.model }}</td>
+          <td>{{ ship.weight_kg }}</td>
+          <td>{{ ship.year_built }}</td>
+        </tr>
+        </tbody>
+        </table>
+      </div>
+    </div> 
   </div>
 </template>
 
 <script>
+
 import gql from 'graphql-tag';
 
 export default {
     name: 'Launch',
     data(){
         return{
-          launch: '',
+          launch: null,
           id: this.$route.params.id,
         }
     },
     apollo: {
-      launch: gql`query Mylaunch($id : ID!) {   
+      launch: {
+        query: gql`
+        query Mylaunch($id : ID!) {   
         launch(id: $id){
-          details
-          launch_year
+          mission_name
+          ships {
+            weight_kg
+            year_built
+            name
+            model
+            }
+          }
+        }`,
+        variables() {
+          return {
+            id:  this.$route.params.id
+          }
         }
-      }`,
-      variables: {
-        id: '109',
       }
     }
   }
